d290ef3afc7267b9c0ccf1db47d43d0866794d9d
Jenil Thakker <jenilkaushik.thakker@sjsu.edu>
2019-11-22 14:17:36 -0800

    adds dApp

---
+++ app.js
@@ -0,0 +1,33 @@
+import web3 from "./web3";
+import dstr from "./dStorage";
+var fs = require('fs');
+import output from "./out/output.txt";
+
+try {
+    var data = fs.readFileSync('output.txt', 'utf8');
+    console.log(data);    
+} catch(e) {
+    console.log('Error:', e.stack);
+}
+
+const ethTx = require('ethereumjs-tx');
+
+const txParams = {
+  nonce: '0x6', // Replace by nonce for your account on geth node
+  gasPrice: '0x09184e72a000', 
+  gasLimit: '0x30000',
+  to: '', 
+  value: data
+};
+
+// Transaction is created
+const tx = new ethTx(txParams);
+const privKey = Buffer.from('', 'hex');
+
+// Transaction is signed
+tx.sign(privKey);
+const serializedTx = tx.serialize();
+const rawTx = '0x' + serializedTx.toString('hex');
+console.log(rawTx)
+
+eth.sendRawTransaction(rawTx);
---
+++ dSotrage.js
@@ -0,0 +1,6 @@
+import web3 from "./web3";
+
+let abi = [{"constant":false,"inputs":[{"internalType":"string","name":"_authTicket","type":"string"},{"internalType":"string","name":"_documentHash","type":"string"},{"internalType":"string","name":"_lookupHash","type":"string"}],"name":"uploadFile","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"blobber","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"authTicket","type":"string"},{"indexed":false,"internalType":"string","name":"documentHash","type":"string"},{"indexed":false,"internalType":"string","name":"lookupHash","type":"string"}],"name":"fileUpload","type":"event"}];
+let address = "";
+
+export default new web3.eth.Contract(abi, address);
